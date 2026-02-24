0baa985f9e1d97d1ba5374c219d44a1d993787fd
Ercan Erguler <ercanerguler@gmail.com>
2026-02-24 20:09:53 +0300

    fix: JWT_EXPIRE fallback to 30d

--- backend/models/User.js
+++ backend/models/User.js
@@ -90,7 +90,7 @@ UserSchema.pre('save', async function (next) {
 // Sign JWT
 UserSchema.methods.getSignedJwtToken = function () {
   return jwt.sign({ id: this._id, role: this.role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
+    expiresIn: process.env.JWT_EXPIRE || '30d',
   });
 };
 
